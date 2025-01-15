71f2098a4e7e13f582e1b46c0ff9db0dcf12d989
Developer-Subrata <subrata.ind2@gmail.com>
2025-01-15 21:28:24 +0530

    x

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
   const typedRef=useRef(null)
   useEffect(()=>{
     const options ={
-      strings:["Welcome To My Profile...","I'm Subrata Mandal a Strong & Energitic Boy..."],
+      strings:["Welcome To My Profile...","I'm Subrata Mandal a Strong & Energetic Boy..."],
       typeSpeed:60,
       backSpeed:30,
       loop:true
